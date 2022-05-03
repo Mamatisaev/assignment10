655850517432df130850f347ea7d1726268defec
Mamatisaev <umamatisaev@gmail.com>
2022-05-03 10:52:22 +0600

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        System.out.println(contains(a, 1, 2, 3, 4, 5, 6, 7, 9, 10));
+        System.out.println(contains(scan.nextInt(), 1, 2, 3, 4, 5, 6, 7, 9, 10));
     }
     public static boolean contains(int a, int... b) {
-
         for (int i = 0; i < b.length; i++) {
             if (a == b [i]) {
                 return true;
